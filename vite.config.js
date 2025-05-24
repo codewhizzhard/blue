366de765f9aecc93486cccc76470310bd5642567
codewhizzhard <topeadeolu5@gmail.com>
2025-05-24 10:22:15 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  
+    base: '/', // Do NOT set this to ./ or anything else unless you're deploying to a subfolder
 })
